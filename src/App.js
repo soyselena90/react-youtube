40101b09a051d26714749ee6b5093221ba8fc6fa
DESKTOP-RVH1HAK\selena <soyselena90@naver.com>
2021-11-03 17:41:44 +0900

    youtube add service

--- src/App.js
+++ src/App.js
@@ -3,32 +3,25 @@ import "./assets/css/reset.css";
 import SearchHeader from "./components/search_header/search_header";
 import VideoList from "./components/video_list/video_list";
 
-function App() {
+function App({ youtube }) {
    //videos의 목록을 가질 수 있는 state 만들기
    const [videos, setVideos] = useState([]);
+   const search = (query) => {
+      youtube
+         .search(query) //
+         .then((videos) => setVideos(videos));
+   };
 
-   // 업데이트(마운트)가 되었을 때 유투브로부터 데이터 받아오기 - 한 번
    useEffect(() => {
-      const requestOptions = {
-         method: "GET",
-         redirect: "follow",
-      };
-
-      fetch(
-         "https://www.googleapis.com/youtube/v3/videos?part=snippet&chart=mostPopular&maxResults=25&key=AIzaSyBhSORu387fEy2EbuT2suGXhEwuSCRY6Ig",
-         requestOptions
-      )
-         .then((response) => response.json()) //json으로 변환
-         //변환된 파일중에 items 부분을 비동기적으로 videos에 update
-         .then((result) => setVideos(result.items))
-         .catch((error) => console.log("error", error));
+      youtube
+         .mostPopular() //
+         .then((videos) => setVideos(videos));
    }, []);
+
    return (
       <div className="App">
-         <>
-            <SearchHeader />
-            <VideoList videos={videos} />
-         </>
+         <SearchHeader onSearch={search} />
+         <VideoList videos={videos} />
       </div>
    );
 }
--- src/components/search_header/search_header.js
+++ src/components/search_header/search_header.js
@@ -1,8 +1,40 @@
-import React from "react";
+import React, { useRef } from "react";
 import styles from "./search_header.module.css";
 
-const SearchHeader = () => {
-   return <header>this is header</header>;
+const SearchHeader = ({ onSearch }) => {
+   const inputRef = useRef();
+   const handleSearch = () => {
+      const value = inputRef.current.value;
+      console.log(value);
+      onSearch(value);
+   };
+   const onKeyPress = (e) => {
+      console.log("onkeypresssss");
+      if (e.key === "Enter") {
+         handleSearch();
+      }
+   };
+   const onClick = () => {
+      console.log("onClick");
+      handleSearch();
+   };
+   return (
+      <header className={styles.hder}>
+         <a href="#!">
+            <img src="./images/logo.png" alt="logo" />
+            <h1>Youtobo</h1>
+         </a>
+         <input
+            ref={inputRef}
+            type="search"
+            placeholder="Search.."
+            onKeyPress={onKeyPress}
+         />
+         <button type="submit" onClick={onClick}>
+            search
+         </button>
+      </header>
+   );
 };
 
 export default SearchHeader;
